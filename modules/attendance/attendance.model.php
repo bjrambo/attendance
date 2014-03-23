702c39e2c89b21fb368d0f32707aebcbff5b06b9
BJRambo <sosifam@070805.co.kr>
2014-03-24 06:57:53 +0900

    #18 코드 최적화 및, 필요없는 스마트폰.php 파일제거

--- modules/attendance/attendance.model.php
+++ modules/attendance/attendance.model.php
@@ -223,10 +223,7 @@ class attendanceModel extends attendance
 		$obj->member_srl = $logged_info->member_srl;
 
 		//관리자 출석이 허가가 나지 않았다면,
-		if($config->about_admin_check == 'no' && $logged_info->is_admin=='Y')
-		{
-			return;
-		}
+		if($config->about_admin_check == 'no' && $logged_info->is_admin=='Y') return;
 
 		/*출석이 되어있는지 확인 : 오늘자 로그인 회원의 DB기록 확인*/
 		if($this->getIsChecked($logged_info->member_srl)>0)
@@ -965,8 +962,7 @@ class attendanceModel extends attendance
 			$end->min = substr($config->end_time,2,2);
 			$now->hour = zDate(date('YmdHis'),"H");
 			$now->min = zDate(date('YmdHis'),"i");
-			if(mktime($now->hour,$now->min,0,0,0) >= mktime($start->hour,$start->min,0,0,0) 
-				&& mktime($now->hour,$now->min,0,0,0) < mktime($end->hour,$end->min,0,0,0))
+			if(mktime($now->hour,$now->min,0,0,0) >= mktime($start->hour,$start->min,0,0,0) && mktime($now->hour,$now->min,0,0,0) < mktime($end->hour,$end->min,0,0,0))
 			{
 				return 1;   //금지시간대일 경우
 			}
